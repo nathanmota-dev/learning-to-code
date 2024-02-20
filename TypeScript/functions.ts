6dc07b58a69ed8a71b84613bbdb168943059d1b4
Nathan <nathansmota@gmail.com>
2024-02-19 23:53:28 -0300

    feat: add objects.ts

---
+++ TypeScript/functions.ts
@@ -0,0 +1,31 @@
+//quando não especificado o tipo de retorno, o TypeScript assume que é o padrão passado como retorno
+const sum = (x: number, y: number) => {
+    return x + y;
+}
+
+const value1 = sum(2, 3);
+
+//porem é possível especificar o tipo de retorno
+const sum2 = (x: number, y: number): string => {
+    return (x + y).toString();
+}
+
+const value2 = sum2(2, 3);
+
+//é possível também especificar mais de um tipo de retorno
+const sum3 = (x: number, y: number): string | number => {
+    return (x + y).toString();
+}
+
+//existe também variáveis opcionais em funções
+const sum4 = (x: number, y: number, z?: number) => {
+    return (x + y + (z || 0)); //caso z não seja passado, assume 0
+} //retornar apenas x+y+z pode causar erro, pois z pode ser undefined
+
+const values = sum4(2, 3, 5);
+
+//é possível também criar uma função void que não retorna nada assim como no  c
+const log = (message: string): void => {
+    console.log(message);
+    //usandor return em uma função void o TypeScript acusa erro
+}
